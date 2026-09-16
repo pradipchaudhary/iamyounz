784f74299a04dc477d108611a932ae7c9cdd93aa
Pradip Chaudhary <chaudharypradip678@gmail.com>
2026-09-17 04:31:02 +0545

    add Footer UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
 
@@ -10,6 +11,8 @@ export default function Home() {
         <Hero />
         
       </main>
+      {/* Footer  */}
+      <Footer />
     </div>
   );
 }
